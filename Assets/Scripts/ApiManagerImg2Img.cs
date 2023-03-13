3e7f1d8d4d65ccc4ba5b3c18658fecb9f7f018ee
Sandrik8 <sandromagradze8@gmail.com>
2023-03-13 19:21:40 +0400

    img2img

--- Assets/Scripts/ApiManagerImg2Img.cs
+++ Assets/Scripts/ApiManagerImg2Img.cs
@@ -8,7 +8,7 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ApiManagerTxt2Img : Singleton<ApiManagerTxt2Img>
+public class ApiManagerImg2Img : Singleton<ApiManagerTxt2Img>
 {
     public string bearer;
     public TMP_InputField promptInput;
@@ -38,18 +38,26 @@ public class ApiManagerTxt2Img : Singleton<ApiManagerTxt2Img>
     public TMP_InputField s_tmin;
     public TMP_InputField s_noise;
     public TMP_InputField sampler_index;
-    
+
 
     [HideInInspector] public string lastDownloadedBase64;
 
-    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
-    public class Txt2ImageRequest : BaseRequest
+
+    public class Image2ImageRequest : BaseRequest
     {
-        public bool enable_hr { get; set; }
-        public int denoising_strength { get; set; }
-        public int firstphase_width { get; set; }
-        public int firstphase_height { get; set; }
+        public List<string> init_images { get; set; }
+        public int resize_mode { get; set; }
+        public double denoising_strength { get; set; }
+
+        public string mask { get; set; }
+        public int mask_blur { get; set; }
+        public int inpainting_fill { get; set; }
+        public bool inpaint_full_res { get; set; }
+        public int inpaint_full_res_padding { get; set; }
+        public bool inpainting_mask_invert { get; set; }
+        
         public string prompt { get; set; }
+
         public List<string> styles { get; set; }
         public int seed { get; set; }
         public int subseed { get; set; }
@@ -70,24 +78,33 @@ public class ApiManagerTxt2Img : Singleton<ApiManagerTxt2Img>
         public int s_tmax { get; set; }
         public int s_tmin { get; set; }
         public int s_noise { get; set; }
+        public OverrideSettingsImg override_settings { get; set; }
         public string sampler_index { get; set; }
+        public bool include_init_images { get; set; }
 
         public override string PostUrl() =>
-            "http://3.137.201.231:8080/sdapi/v1/txt2img";
+            "http://3.137.201.231:8080/sdapi/v1/img2img";
     }
 
+
     // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
-    public class Parameters
+    public class OverrideSettingsImg
     {
-        public bool enable_hr { get; set; }
+    }
+
+    public class ParametersImg
+    {
+        public object init_images { get; set; }
+        public int resize_mode { get; set; }
         public double denoising_strength { get; set; }
-        public int firstphase_width { get; set; }
-        public int firstphase_height { get; set; }
-        public double hr_scale { get; set; }
-        public object hr_upscaler { get; set; }
-        public int hr_second_pass_steps { get; set; }
-        public int hr_resize_x { get; set; }
-        public int hr_resize_y { get; set; }
+        public object image_cfg_scale { get; set; }
+        public object mask { get; set; }
+        public int mask_blur { get; set; }
+        public int inpainting_fill { get; set; }
+        public bool inpaint_full_res { get; set; }
+        public int inpaint_full_res_padding { get; set; }
+        public int inpainting_mask_invert { get; set; }
+        public object initial_noise_multiplier { get; set; }
         public string prompt { get; set; }
         public List<object> styles { get; set; }
         public int seed { get; set; }
@@ -110,29 +127,35 @@ public class ApiManagerTxt2Img : Singleton<ApiManagerTxt2Img>
         public double s_tmax { get; set; }
         public double s_tmin { get; set; }
         public double s_noise { get; set; }
-        public object override_settings { get; set; }
+        public OverrideSettingsImg override_settings { get; set; }
         public bool override_settings_restore_afterwards { get; set; }
         public List<object> script_args { get; set; }
         public string sampler_index { get; set; }
+        public bool include_init_images { get; set; }
         public object script_name { get; set; }
     }
 
-    public class Txt2ImageResponse : BaseResponse
+    public class Image2ImageResponse : BaseResponse
     {
         public List<string> images { get; set; }
-        public Parameters parameters { get; set; }
+        public ParametersImg parameters { get; set; }
         public string info { get; set; }
     }
 
-    public void SendRequestTxt2Img(Action callback)
+
+    public void SendRequestImg2Img(string txt, Action callback)
     {
-        NetCenter.Instance.Send<Txt2ImageResponse>(new Txt2ImageRequest()
+        NetCenter.Instance.Send<Image2ImageResponse>(new Image2ImageRequest()
             {
-                enable_hr = enable_hr.isOn,
+                init_images = { },
+                resize_mode = 0,
                 denoising_strength = Int32.Parse(denoising_strength.text),
-                firstphase_height = Int32.Parse(firstphase_height.text),
-                firstphase_width = Int32.Parse(firstphase_width.text),
-                prompt = promptInput.text,
+                mask_blur = 4,
+                inpainting_fill = 0,
+                inpaint_full_res = false,
+                inpaint_full_res_padding = 0,
+                inpainting_mask_invert = false,
+                styles = { },
                 seed = Int32.Parse(seed.text),
                 subseed = Int32.Parse(subseed.text),
                 subseed_strength = Int32.Parse(subseed_strength.text),
@@ -152,11 +175,13 @@ public class ApiManagerTxt2Img : Singleton<ApiManagerTxt2Img>
                 s_tmax = Int32.Parse(s_tmax.text),
                 s_tmin = Int32.Parse(s_tmin.text),
                 s_noise = Int32.Parse(s_noise.text),
-                sampler_index = sampler_index.text
+                override_settings = { },
+                sampler_index = sampler_index.text,
+                include_init_images = false
             },
             msg =>
             {
-                var response = msg as Txt2ImageResponse;
+                var response = msg as Image2ImageResponse;
                 callback?.Invoke();
                 ConvertFromBase(response.images[0]);
             }, e => { Debug.Log(e); });
@@ -172,27 +197,5 @@ public class ApiManagerTxt2Img : Singleton<ApiManagerTxt2Img>
         Sprite sprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f),
             100.0f);
         UIManager.Instance.centralImage.sprite = sprite;
-    }
-
-    public void SaveImage(Sprite sprite)
-    {
-        // string proj_path = Application.dataPath;
-        // var abs_path = Path.Combine(Application.dataPath, proj_path);
-        // proj_path = Path.Combine("Assets", proj_path);
-        //
-        // Directory.CreateDirectory(Path.GetDirectoryName(abs_path));
-        // File.WriteAllBytes(abs_path, ImageConversion.EncodeToPNG(sprite.texture));
-        //
-        // AssetDatabase.Refresh();
-        //
-        // var ti = AssetImporter.GetAtPath(proj_path) as TextureImporter;
-        // ti.spritePixelsPerUnit = sprite.pixelsPerUnit;
-        // ti.mipmapEnabled = false;
-        // ti.textureType = TextureImporterType.Sprite;
-        //
-        // EditorUtility.SetDirty(ti);
-        // ti.SaveAndReimport();
-
-        //return AssetDatabase.LoadAssetAtPath<Sprite>(proj_path);  //Tu shenaxvis mere amogeba dagvchirda
     }
 }
